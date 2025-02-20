9cafc1c3fade3bcdfeb570ab4bb4554c7ccbe187
Bibalaye7 <ndioneabibou7@gmail.com>
2025-02-20 11:44:58 +0000

    amélioration

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     alert('Merci de nous avoir contacter! Nous vous reviendrons plus tard.');
     this.reset();
 });
+/* Ajouter ce script pour forcer le zoom initial */
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const viewportMeta = document.querySelector('meta[name="viewport"]');
+        viewportMeta.content = "width=820, initial-scale=1.0";
+    });
